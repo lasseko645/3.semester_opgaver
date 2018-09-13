9112c87ba15708ecef1c2e6b873efe968cd7236c
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-09-13 10:34:14 +0200

    jeg har added min client klasse og den vigtigste metode i den klasse (læs comments for documentation)

--- src/krydsogbolle/Client.java
+++ src/krydsogbolle/Client.java
@@ -1,10 +1,12 @@
 package krydsogbolle;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client {
+public class Client extends Thread {
 
 
     //commit:4
@@ -16,6 +18,27 @@ public class Client {
     BufferedReader input;
     PrintWriter output;
     //commit:4 end
+
+
+    //commit:5
+    //I will start with my socket programming here and later get back to the logic of the game itself
+    //i make the individual socket for every player.
+    //my goal with this is to make posible for more than two people two pair up in seperate pairs and play with each other, and ONLY eachother
+    public Client(Socket socket, char x_Or_O){
+        this.socket = socket;
+        this.x_Or_O = x_Or_O;
+        try {
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
+            output.println("Welcome Player:" + x_Or_O);
+            output.println("Waiting for player to connect...");
+        }   catch (Exception e){
+            System.out.println("oponent disconnected" + e);
+        }
+    }
+    //i am not sure if this will do it but it should surfice acording to my documentation
+    //commit:5 end
+
 
 
 
