458060b68d1336fe6792820c79c38d3376234111
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-09-13 09:25:36 +0200

    har oprettet det første del af vores program vores server klasse som kan kompilere...

---
+++ src/krydsogbolle/Serverside.java
@@ -0,0 +1,18 @@
+package krydsogbolle;
+
+import java.net.*;
+
+public class Serverside {
+
+
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket server = new ServerSocket(7777);
+        System.out.println("server is running...");
+
+    }
+
+
+
+
+}
