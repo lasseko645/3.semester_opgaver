774c31f5081e1930b40073958489960cb6988902
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-09-13 14:02:09 +0200

    har added metoden winnerFound som skal gøre are at vores ser ved hvornår en spiller har vundet

--- src/krydsogbolle/TicTacToesGame.java
+++ src/krydsogbolle/TicTacToesGame.java
@@ -6,13 +6,36 @@ public class TicTacToesGame {
     //here i make an array that handle userside information about current board
     //right now this array wont work bacause the Playboard class has not been made yet
     //as default i make all the values nothing
-    private Client[] board {
+    private Client[] gameBoard = {
         null, null, null,
         null, null, null,
         null, null, null};
     //commit:3 end
 
 
+    //commit: 9 start
+    //i will just create the first client as the server
+    Client playerHost;
+    //this will do here for now just to keep track of what needs to get done
+
+
+    //i will create the statement for how to win,
+    //i easily found this logic online but read trough it
+    //and you'll find that its quite simple slavic walktrought of all possible correct cominations
+    public boolean winnerFound(){
+        return (
+                gameBoard[0] != null && gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2])
+                ||(gameBoard[3] != null && gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5])
+                ||(gameBoard[6] != null && gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8])
+                ||(gameBoard[0] != null && gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6])
+                ||(gameBoard[1] != null && gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7])
+                ||(gameBoard[2] != null && gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8])
+                ||(gameBoard[0] != null && gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8])
+                ||(gameBoard[2] != null && gameBoard[2] == gameBoard[4] && gameBoard[2] == gameBoard[6]);
+
+    }
+    //this should do it, unfortunately as of the time of this commit i have no way to test if it works so ill just have to trust in my planning
+    //commit: 9 end
 
 
 
