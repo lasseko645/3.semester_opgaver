65f425cc81a7cbd2c69410e4fb682cbacc3fef9f
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-09-13 09:45:20 +0200

    jeg har added en ny klasse som skal styre client side info control og har oprettet et array som skal bruges senere

---
+++ src/krydsogbolle/TicTacToesGame.java
@@ -0,0 +1,15 @@
+package krydsogbolle;
+
+public class TicTacToesGame {
+
+    //commit:3 start
+    //here i make an array that handle userside information about current board
+    //right now this array wont work bacause the Playboard class has not been made yet
+    //as default i make all the values nothing
+    private PlayBoard[] board {
+        null, null, null,
+        null, null, null,
+        null, null, null};
+    //commit:3 end
+
+}
