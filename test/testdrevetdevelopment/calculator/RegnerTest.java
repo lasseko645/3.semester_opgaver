5db7424485b6ecad1587c2347ec533dd15453785
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-10-02 09:24:42 +0200

    jeg har sat test 1 op som en template til fremtidige tests

--- test/testdrevetdevelopment/calculator/RegnerTest.java
+++ test/testdrevetdevelopment/calculator/RegnerTest.java
@@ -9,12 +9,32 @@ public class RegnerTest {
 
     //  lasse  \\
     //simple test for the first funktion
+    //this first test will funktion as a sort of template for future test
+    //this will be done primarily with comments, so if you dont care for that ignore the first test
+    //the first test will just be for the adition funktion so should be simple enough
     @Test
     private void testAdition(){
+        //datafield: for all local variable and objects that the test will need to funktion
+        //datafield start
 
-        //in the first commit nothing will be here
+
+
+        //datafield end
+
+
+        //method use: potential use of different functions in order to make the test work
+        //method use start
 
 
 
+        //method use end
+
+
+        //assertion: assert equals to make an assumption to make the test respond with a success or a failure
+        //assertion start
+
+
+
+        //assertion end
     }
 }
