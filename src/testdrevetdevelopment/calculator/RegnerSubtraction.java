0a21eaef23a02751097c481415cdc87e47d5e399
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-10-02 10:41:03 +0200

    jeg har udbygget logikken for subtraction og samtidig udviklet subtrakyion klassen
    test: subtraction
    færdig: ja
    grøn bar: ja

---
+++ src/testdrevetdevelopment/calculator/RegnerSubtraction.java
@@ -0,0 +1,9 @@
+package testdrevetdevelopment.calculator;
+
+public class RegnerSubtraction {
+
+
+    public double calcSimpleSub(double a, double b){
+        return a - b;
+    }
+}
--- test/testdrevetdevelopment/calculator/RegnerTest.java
+++ test/testdrevetdevelopment/calculator/RegnerTest.java
@@ -43,11 +43,7 @@ public class RegnerTest {
     @Test
     void testSubtraction(){
 
-        RegnerSubtraction subtraction = new RegnerSubtraction;
-
-
-
-
+        RegnerSubtraction subtraction = new RegnerSubtraction();
 
         assertEquals(6,subtraction.calcSimpleSub(13, 7));
     }
